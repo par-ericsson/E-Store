95f2e31b99a8f287187c6afd106b600bf59dbd74
Pär Ericsson <ericsson@home.se>
2022-03-17 20:15:53 +0100

    Refactor to use redux-toolkit and entity adapters.

--- client/src/features/contact/ContactPage.tsx
+++ client/src/features/contact/ContactPage.tsx
@@ -1,9 +1,24 @@
-import { Typography } from "@mui/material";
+import { Button, ButtonGroup, Typography } from "@mui/material";
+import { Fragment } from "react";
+import { useAppDispatch, useAppSelector } from "../../app/store/configureStore";
+import { decrement, increment } from "./counterSlice";
 
 export default function ContactPage() {
+    const dispatch = useAppDispatch();
+    const {data, title} = useAppSelector(state => state.counter);
+
     return (
-        <Typography variant='h2'>
-            Contact Page
-        </Typography>
+        <Fragment>
+            <Typography variant='h2'>
+                {title}
+            </Typography>
+            <Typography variant='h2'>
+                The date is: {data}
+            </Typography>
+            <ButtonGroup>
+                <Button onClick={() => dispatch(decrement(1))} variant='contained' color='error'>Decrement</Button>
+                <Button onClick={() => dispatch(increment(1))} variant='contained' color='primary'>Decrement</Button>
+            </ButtonGroup>
+        </Fragment>
     )
 }
