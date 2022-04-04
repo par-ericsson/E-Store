1a79454c3f1871834d2dedcf19e3adfe5a9b0be4
Pär Ericsson <ericsson@home.se>
2022-04-04 20:33:15 +0200

    Stripe added for payment

---
+++ API/Data/Migrations/20220403094651_PaymentIntentAdded.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    public partial class PaymentIntentAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentIntentId",
+                table: "Orders",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ClientSecret",
+                table: "Baskets",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentIntentId",
+                table: "Baskets",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "b84ea797-05b4-41b8-8d09-bf1fa3d73a85");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "c50b3298-65fc-497c-ae54-44b174e66779");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaymentIntentId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "ClientSecret",
+                table: "Baskets");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentIntentId",
+                table: "Baskets");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "cc72882f-fbad-4b31-a136-d9f967014ba7");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "ef55c6d6-f30a-43eb-b504-df9c0336998c");
+        }
+    }
+}
---
+++ client/src/features/checkout/CheckoutWrapper.tsx
@@ -0,0 +1,32 @@
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import { useState, useEffect } from "react";
+import agent from "../../app/api/agent";
+import LoadingComponent from "../../app/layout/LoadingComponent";
+import { useAppDispatch } from "../../app/store/configureStore";
+import { setBasket } from "../basket/basketSlice";
+import CheckoutPage from "./CheckoutPage";
+
+const stripePromise = loadStripe("pk_test_51KkPg3F4PWoVQybdKwiZWsubZioWgYRoUdRSMPokgm64xPxqCJSKPcKSY9A0ojvufmJ1Id22KdOvyVZO3eJFYHot00kQTzfOXQ")
+
+export default function CheckoutWrapper() {
+    const dispatch = useAppDispatch();
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        agent.Payments.createPaymentIntent()
+            .then(basket => dispatch(setBasket(basket)))
+            .catch(error => console.log(error))
+            .finally(() => setLoading(false));
+    }, [dispatch]);
+
+    if (loading) {
+        return <LoadingComponent message='Loading checkout...' />
+    }
+
+    return (
+        <Elements stripe={stripePromise}>
+            <CheckoutPage />
+        </Elements>
+    )
+}
